b29e146531424750f988a1f8450be8099688fa20
ishumsk <ivan.shumski@ibm.com>
2024-07-25 18:25:14 +0300

    fix input issue when url is changed

--- src/app/shared/directives/image-button/image-button.directive.ts
+++ src/app/shared/directives/image-button/image-button.directive.ts
@@ -1,15 +1,21 @@
-import { Directive, ElementRef, Renderer2, Input, AfterViewInit } from '@angular/core';
+import { Directive, ElementRef, Renderer2, Input, AfterViewInit, SimpleChanges, OnChanges } from '@angular/core';
 
 @Directive({
   standalone: true,
   selector: '[appImageButton]'
 })
-export class ImageButtonDirective implements AfterViewInit {
+export class ImageButtonDirective implements OnChanges, AfterViewInit {
   @Input('appImageButton') imageUrl!: string;
   @Input('width') width!: string;
   @Input('height') height!: string;
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
+
+  ngOnChanges(simpleChanges: SimpleChanges) {
+    if (simpleChanges['imageUrl'] && !simpleChanges['imageUrl'].isFirstChange()) {
+      this.renderer.setStyle(this.elementRef.nativeElement, 'backgroundImage', `url(${this.imageUrl})`);
+    }
+  }
 
   ngAfterViewInit(): void {
     this.renderer.setStyle(this.elementRef.nativeElement, 'width', this.width + 'px');
