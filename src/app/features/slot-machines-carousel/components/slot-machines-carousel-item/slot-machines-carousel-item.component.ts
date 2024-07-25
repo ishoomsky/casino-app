17715ca2470ae084bc3749ba062eb81af54225f1
ishumsk <ivan.shumski@ibm.com>
2024-07-25 18:55:52 +0300

    style carousel item, badges

--- src/app/features/slot-machines-carousel/components/slot-machines-carousel-item/slot-machines-carousel-item.component.ts
+++ src/app/features/slot-machines-carousel/components/slot-machines-carousel-item/slot-machines-carousel-item.component.ts
@@ -1,16 +1,30 @@
 import { Component, Input } from '@angular/core';
 import { SlotInterface } from "../../../../shared/types/slot.interface";
 import { AnimateNumberDirective } from "../../../../shared/directives/animate-number/animate-number.directive";
+import { NgClass, NgIf, NgStyle, NgTemplateOutlet } from "@angular/common";
+
+interface BadgePosition {
+  top: number;
+}
 
 @Component({
   selector: 'app-slot-machines-carousel-item',
   standalone: true,
   imports: [
-    AnimateNumberDirective
+    AnimateNumberDirective,
+    NgIf,
+    NgTemplateOutlet,
+    NgClass,
+    NgStyle,
   ],
   templateUrl: './slot-machines-carousel-item.component.html',
   styleUrl: './slot-machines-carousel-item.component.scss'
 })
 export class SlotMachinesCarouselItemComponent {
   @Input() slot!: SlotInterface;
+
+  public jackpotBadgePositions: { [slotId: number]: BadgePosition } = {
+    1145: { top: 37 },
+    1148: { top: 46 },
+  };
 }
