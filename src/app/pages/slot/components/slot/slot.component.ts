c25b761a65c0846cd387c5d0179964b20b1b80e7
ishumsk <ivan.shumski@ibm.com>
2024-07-26 14:39:31 +0300

    implement slot machine

--- src/app/pages/slot/components/slot/slot.component.ts
+++ src/app/pages/slot/components/slot/slot.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { AnimateNumberDirective } from "../../../../shared/directives/animate-number/animate-number.directive";
+import { NgForOf } from "@angular/common";
 
 @Component({
   selector: 'app-slot',
   standalone: true,
-  imports: [],
+  imports: [
+    AnimateNumberDirective,
+    NgForOf
+  ],
   templateUrl: './slot.component.html',
   styleUrl: './slot.component.scss'
 })
 export class SlotComponent {
+  public jackpots: {value: number}[] = [
+    {
+      value: 500072001387,
+    },
+    {
+      value: 125100801942,
+    },
+    {
+      value: 35158403052,
+    },
+    {
+      value: 15244804717,
+    },
+    {
+      value: 4731427500,
+    }
+  ];
+
+  public numberSevenBadges =  Array(5).fill(0);
 
 }
